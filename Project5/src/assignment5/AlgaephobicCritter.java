6385c1e80a31cdd093fc117d7b9df5ac0197414f
Shamma Kabir <shammakabir@utexas.edu>
2016-11-08 12:13:17 -0600

    fixed look

--- Project5/src/assignment5/AlgaephobicCritter.java
+++ Project5/src/assignment5/AlgaephobicCritter.java
@@ -11,7 +11,7 @@ public class AlgaephobicCritter extends Critter {
 		Params.look_energy_cost = 0;
 		Params.walk_energy_cost = 0;
 		Params.run_energy_cost = 0;
-		Params.refresh_algae_count = 0;
+		Params.refresh_algae_count = 10;
 	}
 
 	public boolean fight(String not_used) { return false; }
